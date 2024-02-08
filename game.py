b37fc67d8a40bb7da91b5eac96e98e1ce03f748c
benchang <ptst1110.ee10@nycu.edu.tw>
2024-02-08 19:55:16 +0800

    add model plugin

--- game.py
+++ game.py
@@ -1,29 +1,60 @@
 import pygame
 import random
+import os
+import torch
+from PIL import Image
+import shutil  
+import numpy as np
+
+def predict_number(truth,image_path,model_path='demo.pth')->bool:
+    '''
+    truth: int, the truth of the image
+    model_path: str, the path of the model
+    image_path: str, the path of the image 
+    return: bool, if true, the prediction is correct, otherwise, the prediction is wrong
+    '''
+    model = torch.jit.load(model_path)
+    model.eval()
+    image = Image.open(image_path)
+    # preprocess the image
+    image = image.resize((28,28))
+    image = image.convert('L')
+    image = torch.tensor(np.array(image),dtype=torch.float32)
+    image = image / 255
+    image = image.view(1,1,28,28)
+    # predict the number
+    prediction = model(image)
+    prediction = torch.argmax(prediction)
+    # print(prediction,truth)
+    return prediction.item() == truth
+    
+if os.path.exists('screenshots'):
+    shutil.rmtree('screenshots')
+os.makedirs('screenshots')
 
 pygame.init()
 # settint some variables
 white = (255,255,255)
 black = (0,0,0)
 window_width = 560
-window_height = 600
-font_size = 20
-countdown = 10 * 1000
+window_height = 610
+font_size = 30
+countdown = 5 * 1000
 # create some objects for future use
 font = pygame.font.Font(None, font_size) # create a font object
 font.set_bold(False) # make the font bold
 window = pygame.display.set_mode((window_width, window_height)) # create a window
 pygame.display.set_caption("MNIST Game") # set the title of the window
 
-
 # initialize the window
 window.fill((0,0,0)) # fill the window with black color
-pygame.draw.line(window, white, (0, 50), (window_width, 50), 1) # draw a line to separate the top bar
+pygame.draw.line(window, white, (0, window_height-window_width-1), (window_width, window_height-window_width-1), 1) # draw a line to separate the top bar
 start_ticks = pygame.time.get_ticks() # starter tick
 number = random.randint(0,9) # get a random number
 drawing = False
-total_quest = 1
+total_quest = 5
 quest = 1
+correct = 0
 
 # game part
 while True:
@@ -47,19 +78,23 @@ while True:
         if drawing:
             pygame.draw.circle(window, (255,255,255), pygame.mouse.get_pos(), radius = 20)
     else:
-        seconds = 0
+        seconds = 0        
+        screenshot_area = pygame.Rect(0,window_height-window_width,window_width,window_width) # Define the area of the screenshot
+        screenshot = window.subsurface(screenshot_area) # Capture a screenshot of the specific section
+        pygame.image.save(screenshot, os.path.join('screenshots', 'screenshot_{}.png'.format(quest))) # Save the screenshot
+        correct += predict_number(number,os.path.join('screenshots', 'screenshot_{}.png'.format(quest)))
         start_ticks = pygame.time.get_ticks() # reset the timer
         number = random.randint(0,9) # reset the number
         quest += 1
         # initialize the window
         window.fill((0,0,0)) # fill the window with black color
-        pygame.draw.line(window, white, (0, 50), (window_width, 50), 1) # draw a line to separate the top bar
+        pygame.draw.line(window, white, (0, window_height-window_width-1), (window_width, window_height-window_width-1), 1) # draw a line to separate the top bar
 
     # set the place of the text
     timer_text_rect = timer_text.get_rect(topright = (window_width,0))
     number_text_rect = number_text.get_rect(topleft = (0,0))
     # remoove the top bar
-    fill_ract = (0,0,window_width,50)
+    fill_ract = (0,0,window_width,window_height-window_width-1)
     window.fill(black, fill_ract)
     # draw the text
     window.blit(timer_text, timer_text_rect)
@@ -70,7 +105,7 @@ while True:
     # check if the game is over
     if quest == total_quest + 1:
         window.fill(black)
-        finish_text = font.render("Game Over", True, white)
+        finish_text = font.render("You get {} out of {}".format(correct,total_quest), True, white)
         finish_text_rect = finish_text.get_rect(center = (window_width/2, window_height/2))
         window.blit(finish_text, finish_text_rect)
         pygame.display.update()
@@ -78,11 +113,3 @@ while True:
         pygame.quit()
         quit()
 
-    
-    
-    
-    
-    
-#     number = 2
-# text = font.render("Please draw {}".format(number), True, white) # create a text object
-# text_react = text.get_rect(topleft = (0,0))
